8b9aca5fdd57beaba68d73761c77722155f561cf
gavrilovds <dimagavrilov2004@yandex.ru>
2023-11-04 15:04:03 +0300

    genres: add info about current playing song

--- scripts/playMusic.js
+++ scripts/playMusic.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const audioPlayer = new Audio();
     let currentAudio = null;
     let currentButton = null;
+    const notification = document.getElementById('notification');
+    const songInfo = document.getElementById('song-info');
 
     playButtons.forEach(button => {
         button.addEventListener('click', function () {
             const audioId = button.getAttribute('data-audio');
             const audio = document.getElementById(audioId);
+            const currentSongName = button.getAttribute('data-song-name');
 
             if (currentAudio && currentAudio !== audio) {
                 currentAudio.pause();
@@ -20,13 +23,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 button.textContent = 'Пауза';
                 currentAudio = audio;
                 currentButton = button;
+                showNotification(currentSongName);
             } else {
                 audio.pause();
                 audio.currentTime = 0;
                 button.textContent = 'Послушать';
                 currentAudio = null;
                 currentButton = null;
+                hideNotification();
             }
         });
     });
+
+    function showNotification(text) {
+        songInfo.textContent = `Сейчас играет: ${text}`;
+        notification.style.display = 'block';
+    }
+
+    function hideNotification() {
+        notification.style.display = 'none';
+    }
 });
